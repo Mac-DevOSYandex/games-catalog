da644f4660f16d6c2d3e006969a768e98ddfa2be
Gio <MacDev-Admin@protonmail.com>
2021-11-22 15:52:36 +0000

    ci: add CI workflow, add super-linter (#1)
    * chore: organise workspace configuration
    
    * ci: add build job
    
    * error: sth went wrong in last commit could not push
    
    * docs: update readme
    
    * docs: upate readme
    
    * chore: correct typo in yml
    
    * chore: amend yml
    
    * docs: update
    
    * chore: update tsconfig include dependencies version
    
    * ci: add super-linter & test job
    
    * docs: update
    
    * env: add package versions
    
    * ci:lint: amend job
    
    * ci:lint: add GITHUB_TOKEN

--- apps/games-catalog-app/src/environments/environment.prod.ts
+++ apps/games-catalog-app/src/environments/environment.prod.ts
@@ -1,3 +1,24 @@
+import packageJson from '../../../../package.json';
+
 export const environment = {
+  appName: 'Games Catalog - Angular Material AppStarter',
   production: true,
+  envName: 'PROD',
+  test: false,
+  // BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com/games',
+  // HOST: 'rawg-video-games-database.p.rapidapi.com',
+  // KEY: '',
+  //
+  versions: {
+    app: packageJson.version,
+    angular: packageJson.dependencies['@angular/core'],
+    // material: packageJson.dependencies['@angular/material'],
+    angularCore: packageJson.dependencies['@angular/core'],
+    eslint: packageJson.devDependencies['eslint'],
+    rxjs: packageJson.dependencies['rxjs'],
+    cypress: packageJson.devDependencies['cypress'],
+    jest: packageJson.devDependencies['jest'],
+    nrwlCli: packageJson.devDependencies['@nrwl/cli'],
+    typescript: packageJson.devDependencies['typescript'],
+  }
 };
--- apps/games-catalog-app/src/environments/environment.ts
+++ apps/games-catalog-app/src/environments/environment.ts
@@ -1,9 +1,35 @@
+import packageJson from '../../../../package.json';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+/**
+ * `package.json dependebcies can only be default-imported by setting the
+ * flags: "resolveJsonModule": true, & "allowSyntheticDefaultImports": true,
+ * in tscongif.json
+ */
 
 export const environment = {
+  appName: 'Games Catalog - Angular Material AppStarter',
   production: false,
+  envName: 'DEV',
+  test: false,
+  // BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com/games',
+  // HOST: 'rawg-video-games-database.p.rapidapi.com',
+  // KEY: '',
+  //
+  versions: {
+    app: packageJson.version,
+    angular: packageJson.dependencies['@angular/core'],
+    // material: packageJson.dependencies['@angular/material'],
+    angularCore: packageJson.dependencies['@angular/core'],
+    eslint: packageJson.devDependencies['eslint'],
+    rxjs: packageJson.dependencies['rxjs'],
+    cypress: packageJson.devDependencies['cypress'],
+    jest: packageJson.devDependencies['jest'],
+    nrwlCli: packageJson.devDependencies['@nrwl/cli'],
+    typescript: packageJson.devDependencies['typescript'],
+  }
 };
 
 /*
